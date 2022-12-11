5485138e1853e9186c7a317099c006d25ca6575e
mario-oberwalder <mario.oberwalder@gmail.com>
2022-12-11 17:43:39 +0100

    day11 initial commit

---
+++ rust/day11/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn process_part1(input: &str) -> String {
+
+    return result.to_string();
+}
+
+pub fn process_part2(input: &str) -> String {
+
+
+    return pixels;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn when_input_then() {}
+}
